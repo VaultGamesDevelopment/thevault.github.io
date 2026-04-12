8bd31c0541a2766837d3d108209b3b509ba0da06
oblivionxdevs <zachprice173@gmail.com>
2026-04-12 13:12:49 -0400

    er

--- assets/index-DD4QCOMH.js
+++ assets/index-DD4QCOMH.js
@@ -10229,7 +10229,7 @@ Error generating stack: ` + e.message + `
                 onClose: () => a(null)
             }), (0, x.jsx)(`audio`, {
                 ref: S,
-                src: `./bgmusic.mp3`,
+                src: `VaultGamesDevelopment/thevault.github.io/assets/bgmusic.mp3`,
                 loop: !0
             })]
         })
